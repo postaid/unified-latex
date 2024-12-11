48ec0d9bb740917b1391293d31cf88f5131195af
postaid <postaid86@gmail.com>
2024-12-11 14:05:08 +0500

    move to vizex_ru org

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    "parser": '@typescript-eslint/parser',
-    "plugins": ['@typescript-eslint'],
-    "root": true,
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-namespace": "off",
-        "@typescript-eslint/ban-types":"off",
-        "no-case-declarations": "off",
-        "prefer-const": "off",
-        "no-empty": "off",
-        "curly": "error"
-    }
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  "parser": '@typescript-eslint/parser',
+  "plugins": ['@typescript-eslint'],
+  "root": true,
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/ban-types": "off",
+    "no-case-declarations": "off",
+    "prefer-const": "off",
+    "no-empty": "off",
+    "curly": "error"
+  }
 };
